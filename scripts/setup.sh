342b7864b9723e839e2f472f23374d5dc85c5e91
Attila Molnar <ate.molnar2@gmail.com>
2018-05-29 20:32:47 +0200

    Emacs starter script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,3 +4,8 @@
 xdotool key Num_Lock
 xsetroot -cursor_name left_ptr
 xsetroot -solid "#272822"
+
+# Make modifier keys act as regular keys when pressed and released alone under a time limit
+# but before if there is a previously running xcape process kill it.
+killall -9 xcape
+xcape -t 150 -e "Hyper_L=Insert;Alt_L=Tab;Alt_R=Return"
